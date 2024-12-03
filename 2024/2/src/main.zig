4c38c21356db0b0abcbfe897c62eb203073d580c
Christopher Skane <sokrbball@gmail.com>
2024-12-02 23:31:38 -0500

    Completed day 2 part 2.

--- 2024/2/src/main.zig
+++ 2024/2/src/main.zig
@@ -58,33 +58,90 @@ pub fn main() !void {
 
         // Test split on commas (simple CSV)
         _ = try split(&fields, line, ' ');
+        defer fields.clearRetainingCapacity(); // Clear fields each loop
 
-        var prod: isize = 0;
-        var diff: isize = 0;
-        var row_safe: bool = true;
-        var j: usize = 1;
-        while (j < fields.items.len) : (j += 1) {
-            const b = try std.fmt.parseInt(isize, fields.items[j], 10);
-            const a = try std.fmt.parseInt(isize, fields.items[j - 1], 10);
-            diff = b - a;
-            prod = diff * prod;
-            if (prod < 0 or @abs(diff) < 1 or @abs(diff) > 3) {
-                row_safe = false;
+        // Array of tuple (range_start, range_len, is_increasing)
+        var nums = std.ArrayList(isize).init(alloc);
+        defer nums.deinit();
+
+        // Convert whole row to array of numbers
+        for (fields.items) |field| {
+            const v = std.fmt.parseInt(isize, field, 10) catch break;
+            try nums.append(v);
+        }
+
+        var short_list = std.ArrayList(isize).init(alloc);
+        defer short_list.deinit();
+
+        // Skip first element
+        try short_list.appendSlice(nums.items[1..]);
+
+        var j: usize = 0;
+        var k: usize = 1;
+        var increasing: bool = false;
+        var has_safe: bool = true;
+        var last_num: isize = nums.items[0]; // Since we skipped first number
+        while (j < nums.items.len) : (j += 1) {
+            k = 1;
+            has_safe = true;
+            while (k < short_list.items.len) : (k += 1) {
+                const diff = short_list.items[k] - short_list.items[k - 1];
+
+                // If starting new range
+                if (k == 1) {
+                    if (diff < 0) {
+                        increasing = false;
+                    } else {
+                        increasing = true;
+                    }
+                }
+
+                // If difference is outside tolerance, end range.
+                if ((increasing and (diff < 1 or diff > 3)) or (!increasing and (diff < -3 or diff > -1))) {
+                    has_safe = false;
+                    break;
+                }
+            }
+
+            if (has_safe) {
                 break;
             }
 
-            if (diff >= 0) {
-                prod = 1;
-            } else {
-                prod = -1;
+            // For next loop, append removed number then swap remove with next index.
+            try short_list.append(last_num);
+            last_num = short_list.swapRemove(j);
+        }
+
+        // If removing an item yields safe list, then safe by definition.
+        if (has_safe) {
+            safe += 1;
+        } else {
+            // Else try full list
+            k = 1;
+            has_safe = true;
+            while (k < nums.items.len) : (k += 1) {
+                const diff = nums.items[k] - nums.items[k - 1];
+
+                // If starting new range
+                if (k == 1) {
+                    if (diff < 0) {
+                        increasing = false;
+                    } else {
+                        increasing = true;
+                    }
+                }
+
+                // If difference is outside tolerance, end range.
+                if ((increasing and (diff < 1 or diff > 3)) or (!increasing and (diff < -3 or diff > -1))) {
+                    has_safe = false;
+                    break;
+                }
+            }
+
+            if (has_safe) {
+                safe += 1;
             }
         }
-
-        if (row_safe) {
-            safe += 1;
-        }
-
-        fields.clearRetainingCapacity();
     } else |_| {
         try stdout.print("Error\n", .{});
     }
