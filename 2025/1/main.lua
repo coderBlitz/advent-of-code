9f88ba40442860c0218c57601486400e4a76e07b
Christopher Skane <sokrbball@gmail.com>
2025-12-01 23:25:30 -0500

    Finished day 1 part 2.

--- 2025/1/main.lua
+++ 2025/1/main.lua
@@ -11,21 +11,33 @@ for line in io.lines(arg[1]) do
 	local _, _, dir, val = string.find(line, "(%a)(%d+)")
 
 	-- Convert number and normalize to dial size
-	local num = tonumber(val, 10) % DIAL_SIZE
+	local num = tonumber(val, 10)
+	local num_mod = num % DIAL_SIZE
+	local rotations = num // DIAL_SIZE
 
 	-- Do math based on number
+	--print(pos, num_mod, rotations)
 	if dir == 'L' then
-		pos = pos - num
-		if pos < 0 then
+		pos = pos - num_mod
+		if pos == -num_mod then
+			-- If already at 0, not a "pass".
 			pos = pos + DIAL_SIZE
+		elseif pos < 0 then
+			pos = pos + DIAL_SIZE
+			zero_count = zero_count + 1
+		elseif pos == 0 then
+			zero_count = zero_count + 1
 		end
 	else
-		pos = (pos + num) % DIAL_SIZE
+		pos = pos + num_mod
+
+		if pos >= DIAL_SIZE then
+			pos = pos % DIAL_SIZE
+			zero_count = zero_count + 1
+		end
 	end
 
-	if pos == 0 then
-		zero_count = zero_count + 1
-	end
+	zero_count = zero_count + rotations
 end
 
 print("Zero count is", zero_count)
