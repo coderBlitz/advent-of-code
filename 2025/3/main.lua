9e53e7ef1729575eebcecb73f11f9b2494623ee4
Christopher Skane <sokrbball@gmail.com>
2025-12-03 22:06:32 -0500

    Completed day 3 part 2.

--- 2025/3/main.lua
+++ 2025/3/main.lua
@@ -3,44 +3,60 @@ if #arg < 1 then
 	os.exit(0, true)
 end
 
+local REQ_BATS = 12
+
 -- Read file
 local line_num = 0
 local joltage_total = 0
 for line in io.lines(arg[1]) do
 	local line_len = string.len(line)
 
-	-- Start with left-most and right-most for choices
-	local left_digit = string.sub(line, 1,1)
-	local left_idx = 1
-	local right_digit = string.sub(line, -1, -1)
-	local right_idx = line_len
+	local batteries = {
+		[0] = {
+			idx = 0,
+			value = '0'
+		}
+	}
 
-	-- From left to right, find highest.
-	for i = 2, line_len - 1 do
-		c = string.sub(line, i, i)
-		if c > left_digit then
-			left_digit = c
-			left_idx = i
+	for bat = 1, REQ_BATS do
+		-- Next battery must be at least 1 digit to right of last battery.
+		p = batteries[bat - 1].idx + 1
+		--print("Bat", bat, "starting at", p)
+		battery = {
+			idx = p,
+			value = string.sub(line, p, p)
+		}
+
+		-- For remainder of batteries, find highest
+		local bats_remaining = REQ_BATS - bat
+		for i = p, line_len - bats_remaining do
+			-- If this battery has a strictly higher value, replace.
+			-- This ensures left-most value is kept if same.
+			b = string.sub(line, i, i)
+			if b > battery.value then
+				battery.value = b
+				battery.idx = i
+			end
 		end
+
+		table.insert(batteries, battery)
 	end
 
-	-- From right to left, find highest.
-	--for i = 2, line_len, -1 do
-	for i = line_len - 1, left_idx + 1, -1 do
-		c = string.sub(line, i, i)
-		if c > right_digit then
-			right_digit = c
-			right_idx = i
-		end
+	-- Debug print
+	bat_digits = ''
+	for _,b in ipairs(batteries) do
+		bat_digits = bat_digits .. b.value
 	end
+	--print("Digits:", bat_digits)
 
-	-- Convert digits to number and add to total
-	local val = tonumber(left_digit) * 10 + tonumber(right_digit)
-	--print("Line", line_num, "highest:", val)
-
-	joltage_total = joltage_total + val
+	local partial_sum = 0
+	for i = 1, REQ_BATS do
+		partial_sum = partial_sum + 10 ^ (REQ_BATS - i) * tonumber(batteries[i].value)
+	end
+	--print("Bat value:", partial_sum)
+	joltage_total = joltage_total + partial_sum
 
 	line_num = line_num + 1
 end
 
-print("Total joltage:", joltage_total)
+print(string.format("Total joltage: %d", joltage_total))
