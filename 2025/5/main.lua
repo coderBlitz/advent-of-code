a8347a708180bca3db73c0b80b4286ea9fbf2aa9
Christopher Skane <sokrbball@gmail.com>
2025-12-07 17:33:07 -0500

    Completed day 5 part 2.

--- 2025/5/main.lua
+++ 2025/5/main.lua
@@ -39,3 +39,32 @@ for _, av in ipairs(available) do
 end
 
 print("Fresh & avail =", fresh_avail)
+
+-- Part 2
+function range_sort(a,b)
+    return a[1] < b[1]
+end
+table.sort(ranges, range_sort)
+
+-- Simplify ranges
+local combo_ranges = {}
+local total_fresh = 0
+local cur_start = ranges[1][1]
+local cur_end = ranges[1][2]
+for _,r in ipairs(ranges) do
+    if r[1] >= cur_start and r[1] <= cur_end then
+        if r[2] > cur_end then
+            cur_end = r[2]
+        end
+    elseif r[1] > cur_end then
+        table.insert(combo_ranges, {cur_start, cur_end})
+        cur_start = r[1]
+        cur_end = r[2]
+    end
+end
+table.insert(combo_ranges, {cur_start, cur_end})
+
+for _,r in ipairs(combo_ranges) do
+    total_fresh = total_fresh + (r[2] - r[1]) + 1
+end
+print(string.format("Total fresh = %d", total_fresh))
